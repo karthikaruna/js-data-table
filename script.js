c0852120f7613bab4a26d7df9950d95b86b8042e
Karthikeyan Arumugam <karthikeyan.arumugam@freshworks.com>
2019-09-28 13:19:14 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,39 @@
+fetch('https://restcountries.eu/rest/v2/all')
+  .then(response => response.json())
+  .then(data => {
+    new DataTable({
+      mountingElement: document.querySelector('#data-table-mount'),
+      headings: [
+        {
+          id: 'name',
+          type: 'string',
+          label: 'Name'
+        },
+        {
+          id: 'capital',
+          type: 'string',
+          label: 'Capital'
+        },
+        {
+          id: 'region',
+          type: 'string',
+          label: 'Region'
+        },
+        {
+          id: 'population',
+          type: 'number',
+          label: 'Population'
+        },
+        {
+          id: 'area',
+          type: 'number',
+          label: 'Area'
+        }
+      ],
+      data,
+      sortableColumns: ['name', 'capital', 'region', 'population', 'area'],
+      filterableColumns: ['name', 'capital', 'region', 'population', 'area'],
+      isHeaderFixed: true,
+      isPaginated: true
+    })
+  })
